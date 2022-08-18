3cd66295d1476158accf10f6e96a37f9eb058913
darko.deuric <darko.deuric@ethernal.tech>
2022-08-18 12:17:09 +0200

    Expand state with poll objects

--- src/state.rs
+++ src/state.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
@@ -10,4 +10,26 @@ pub struct State {
     pub owner: Addr,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub admin: Addr
+}
+// Derive JSON serialization
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Poll {
+    pub creator: Addr,
+    pub question: String,
+    pub options: Vec<(String, u64)>,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Ballot {
+    pub option: String,
+}
 pub const STATE: Item<State> = Item::new("state");
+// A map with a String key and Poll value.
+// The key will be a UUID generated clientside
+pub const POLLS: Map<String, Poll> = Map::new("polls");
+pub const BALLOTS: Map<(Addr, String), Ballot> = Map::new("ballots");
+
+pub const CONFIG: Item<Config> = Item::new("config");
+
